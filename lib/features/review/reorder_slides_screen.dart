b41439c714d24c853887eaef98cd882691c19557
Alion <alion@example.com>
2026-01-03 01:46:10 +0000

    Review polish: thumbnails, approval guard, slide reordering

---
+++ lib/features/review/reorder_slides_screen.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ReorderSlidesScreen extends StatefulWidget {
+  final String deckId;
+  final List<String> urls;
+
+  const ReorderSlidesScreen({
+    super.key,
+    required this.deckId,
+    required this.urls,
+  });
+
+  @override
+  State<ReorderSlidesScreen> createState() =>
+      _ReorderSlidesScreenState();
+}
+
+class _ReorderSlidesScreenState
+    extends State<ReorderSlidesScreen> {
+  late List<String> _slides;
+
+  @override
+  void initState() {
+    super.initState();
+    _slides = List.from(widget.urls);
+  }
+
+  Future<void> _save() async {
+    await FirebaseFirestore.instance
+        .collection('decks')
+        .doc(widget.deckId)
+        .update({
+      'slideImageUrls': _slides,
+      'coverImageUrl': _slides.first,
+      'updatedAt': FieldValue.serverTimestamp(),
+    });
+
+    if (mounted) Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reorder slides'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: _save,
+          ),
+        ],
+      ),
+      body: ReorderableListView.builder(
+        padding: const EdgeInsets.all(12),
+        itemCount: _slides.length,
+        onReorder: (oldIndex, newIndex) {
+          if (newIndex > oldIndex) newIndex--;
+          setState(() {
+            final item = _slides.removeAt(oldIndex);
+            _slides.insert(newIndex, item);
+          });
+        },
+        itemBuilder: (_, i) => ListTile(
+          key: ValueKey(_slides[i]),
+          leading: Image.network(
+            _slides[i],
+            width: 56,
+            fit: BoxFit.cover,
+          ),
+          title: Text('Slide ${i + 1}'),
+          trailing: const Icon(Icons.drag_handle),
+        ),
+      ),
+    );
+  }
+}
